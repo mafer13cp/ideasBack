7f1d3ddec44470cda3f6255ba24e7e5507f05b7b
Maria Fernanda <mafer13cp@gmail.com>
2022-01-15 01:20:57 -0700

    imgUrl added

--- routes/ideas.js
+++ routes/ideas.js
@@ -29,12 +29,12 @@ router.get("/:id", async(req, res) => {
 
 //POST
 router.post("/post", async(req, res) => {
-    let sql = "INSERT INTO `idea` (`id`, `date`, `summary`, `asignees`, `workflow`, `reviewScore`, `reviewNumber`, `user`, `anonymous`) VALUES (NULL, ?,?,?,?,?,?,?,?)";
-    const {date, summary, asignees, workflow, reviewScore, reviewNumber, user, anonymous} = req.body;
+    let sql = "INSERT INTO `idea` (`id`, `date`, `summary`, `asignees`, `workflow`, `reviewScore`, `reviewNumber`, `user`, `anonymous`, `imgUrl`) VALUES (NULL, ?,?,?,?,?,?,?,?,?)";
+    const {date, summary, asignees, workflow, reviewScore, reviewNumber, user, anonymous, imgUrl} = req.body;
     try {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.status(200).send(await connection.query(sql, [date,summary,asignees,workflow,reviewScore,reviewNumber,user,anonymous]));
+        res.status(200).send(await connection.query(sql, [date,summary,asignees,workflow,reviewScore,reviewNumber,user,anonymous, imgUrl]));
     } catch (error) {
         console.log(error);
     }
@@ -42,12 +42,12 @@ router.post("/post", async(req, res) => {
 
 //PUT (UPDATE)
 router.put("/update/:id", async(req, res) => {
-    let sql = "UPDATE `idea` SET `date` = ?, `summary` = ?, `asignees` = ?, `workflow` = ?, `reviewScore` = ?, `reviewNumber` = ?, `user` = ?, `anonymous` = ? WHERE `id` = " + req.params.id;
-    const {date, summary, asignees, workflow, reviewScore, reviewNumber, user, anonymous} = req.body;
+    let sql = "UPDATE `idea` SET `date` = ?, `summary` = ?, `asignees` = ?, `workflow` = ?, `reviewScore` = ?, `reviewNumber` = ?, `user` = ?, `anonymous` = ?, `imgUrl` = ? WHERE `id` = " + req.params.id;
+    const {date, summary, asignees, workflow, reviewScore, reviewNumber, user, anonymous, imgUrl} = req.body;
     try {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.status(200).send(await connection.query(sql, [date,summary,asignees,workflow,reviewScore,reviewNumber,user,anonymous]));
+        res.status(200).send(await connection.query(sql, [date,summary,asignees,workflow,reviewScore,reviewNumber,user,anonymous, imgUrl]));
     } catch (error) {
         console.log(error);
     }
